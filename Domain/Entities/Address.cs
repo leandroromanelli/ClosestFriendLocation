4644d6cf7791c33d544741faf93d668ed26c6a7a
Leandro Romanelli <leandroromanelli@yahoo.com.br>
2018-07-04 09:11:16 -0300

    melhoria da domain

---
+++ Domain/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ Domain/Repositories/IFriendRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repositories
+{
+    public interface IFriendRepository : IRepository<Friend>
+    {
+        IEnumerable<Friend> GetComplete();
+    }
+}
